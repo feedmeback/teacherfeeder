7b49985eee0f1c88ec54f708fa4395785b5ffd07
franzmoro <franzmoro@hotmail.com>
2016-02-19 17:54:09 +0000

    averages calculating

--- calculation.js
+++ calculation.js
@@ -3,9 +3,9 @@ engagementArr = studentFeedback.Math101["19/02/16"].engagement;
 satisfactionArr = studentFeedback.Math101["19/02/16"].satisfaction;
 supportArr  = studentFeedback.Math101["19/02/16"].support;
 
-engagementTot = engagementArr.reduce(function(item1,item2){ return item1+item2 });
-satisfactionTot = satisfactionArr.reduce(function(item1,item2){ return item1+item2 });
-supportTot = supportArr.reduce(function(item1,item2){ return item1+item2 });
+engagementTot = engagementArr.reduce(function(item1,item2){ return item1+item2 }) / engagementArr.length;
+satisfactionTot = satisfactionArr.reduce(function(item1,item2){ return item1+item2 }) / satisfactionArr.length;
+supportTot = supportArr.reduce(function(item1,item2){ return item1+item2 }) / supportArr.length;
 
 module.exports = {
     engagement: engagementTot,
